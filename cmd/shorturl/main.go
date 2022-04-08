afd8e99a90b1b4bac67588dec64b9dc0664bcbdb
txya900619 <wayne900619@gmail.com>
2022-04-08 18:35:35 +0800

    fix: NewGoCqlxClient fail when KEYSPACE not created

--- cmd/shorturl/main.go
+++ cmd/shorturl/main.go
@@ -20,11 +20,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err = gocqlxClient.ExecStmt("CREATE KEYSPACE IF NOT EXISTS shorturl WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};")
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	err = gocqlxClient.ExecStmt("CREATE TABLE IF NOT EXISTS shorturl.shorturl ( id text PRIMARY KEY, expire_at timestamp, origin_url text);")
 	if err != nil {
 		log.Fatal(err)
--- pkg/gocqlx/session.go
+++ pkg/gocqlx/session.go
@@ -9,17 +9,30 @@ import (
 	"github.com/spf13/viper"
 )
 
-func NewGoCqlxClient() (*gocqlx.Session, error) {
+func newGoCqlxClientWithKeyspace(keyspace string) (*gocqlx.Session, error) {
 	hosts := strings.Split(viper.GetString("CASSANDRA_HOSTS"), ",")
-	fmt.Println(viper.GetString("CASSANDRA_HOSTS"))
 	cluster := gocql.NewCluster(hosts...)
 	cluster.Authenticator = gocql.PasswordAuthenticator{
 		Username: viper.GetString("CASSANDRA_USERNAME"),
 		Password: viper.GetString("CASSANDRA_PASSWORD"),
 	}
 	cluster.ProtoVersion = 4
-	cluster.Keyspace = viper.GetString("CASSANDRA_KEYSPACE")
+	cluster.Keyspace = keyspace
 	session, err := gocqlx.WrapSession(cluster.CreateSession())
 
 	return &session, err
+}
+
+func NewGoCqlxClient() (*gocqlx.Session, error) {
+	systemSession, err := newGoCqlxClientWithKeyspace("system")
+	if err != nil {
+		return nil, err
+	}
+
+	err = systemSession.ExecStmt(fmt.Sprintf("CREATE KEYSPACE IF NOT EXISTS %s WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};", viper.GetString("CASSANDRA_KEYSPACE")))
+	if err != nil {
+		return nil, err
+	}
+
+	return newGoCqlxClientWithKeyspace(viper.GetString("CASSANDRA_KEYSPACE"))
 }
